965fbd96f79e895518bab1e98fd76f868b6b298e
cyberraff <cyberraff@gmail.com>
2024-11-14 03:01:59 +0100

    "Refactor Services page and Footer component: updated imports, formatting, and minor text changes"

--- app/(site)/Services/page.tsx
+++ app/(site)/Services/page.tsx
@@ -1,12 +1,11 @@
-import {client, urlFor} from "@/sanity/config/sanity.utils";
-import React from "react";
-import Image from "next/image";
+import { client, urlFor } from '@/sanity/config/sanity.utils';
+import React from 'react';
+import Image from 'next/image';
 
 async function getData() {
 	const query = `*[_type == "aboutImg"][0]`;
 	return await client.fetch(query);
 }
-
 
 export default async function Services() {
 	const data = await getData();
@@ -25,23 +24,26 @@ export default async function Services() {
 			/>
 			<div className=' px-8 md:px-16 lg:px-36'>
 				<p className='pt-8 px-4 text-xl leading-relaxed '>
-					At FLAYMZE STUDIO, we provide expert service production for shoots in Nigeria,
-					captivating African locations, Canada, and select European countries. Our capabilities span
-					corporate events, music videos, documentaries, weddings, commercials, photo shoots, and
-					more.
+					At FLAYMZE STUDIO, we provide expert service production for
+					shoots in Nigeria, captivating African locations, Canada,
+					and select European countries. Our capabilities span
+					corporate events, music videos, documentaries, weddings,
+					commercials, photo shoots, and more.
 				</p>
 				<p className='text-lg'>
-					The African narrative is changing, and our continent's unique textures, colors, people, and
-					untapped locations are now an envy globally. Our seasoned team, comprising skilled camera
-					crews, producers, location managers, and production designers, ensures top-notch
-					production that meets international standards.
-
+					The African narrative is changing, and our continent&apos;s
+					unique textures, colors, people, and untapped locations are
+					now an envy globally. Our seasoned team, comprising skilled
+					camera crews, producers, location managers, and production
+					designers, ensures top-notch production that meets
+					international standards.
 				</p>
 				<p>
-					Check out our portfolio and get inspired! Let's collaborate to bring your vision to life. Contact
-					us today for tailored service production solutions.
+					Check out our portfolio and get inspired! Let&apos;s
+					collaborate to bring your vision to life. Contact us today
+					for tailored service production solutions.
 				</p>
 			</div>
 		</div>
-	)
+	);
 }
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,10 @@
 'use client';
 import React from 'react';
-import { usePathname } from 'next/navigation';
-import { Facebook, Instagram, Mail, Phone, Twitter } from 'lucide-react';
+// import { usePathname } from 'next/navigation';
+import { Instagram, Twitter } from 'lucide-react';
 import { FaTiktok, FaWhatsapp } from 'react-icons/fa6';
 
-import { links } from './Navbar';
+// import { links } from './Navbar';
 import Link from 'next/link';
 
 const socials = [
@@ -18,7 +18,6 @@ const socials = [
 ];
 
 export default function Footer() {
-
 	return (
 		<footer className=''>
 			{/**/}
