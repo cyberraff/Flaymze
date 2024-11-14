8ee713891cc8887dd380960979f473bfb32e0b9c
cyberraff <cyberraff@gmail.com>
2024-11-14 05:07:26 +0100

    "Updated About and Services pages with new layout and styling changes."

--- app/(site)/About/page.tsx
+++ app/(site)/About/page.tsx
@@ -17,137 +17,147 @@ export default async function About() {
 	const emailAddress = 'email';
 	// console.log(data)
 	return (
-		<div className=' px-4 sm:px-6 max-w-5xl py-2 bg-background lg:max-w-7xl  mx-auto  text-center'>
-			<h1 className='text-3xl font-medium pt-4 pb-4'>ABOUT US</h1>
+		<>
+			<h1 className='text-3xl font-medium pt-8 pb-4 text-center'>
+				ABOUT US
+			</h1>
+			{/* <div className=' relative w-[85%] mx-auto overflow-hidden pb-[46.25%]'> */}
 			<Image
 				src={urlFor(data.image).url()}
 				alt='Great Image'
 				width={450}
 				height={450}
 				priority
-				// className="embla__slide__img "
-				className=' w-full md:mt-8'
+				// className=' absolute top-0 left-0 w-full h-full object-cover '
+				className='w-full h-[30rem] sm:w-[95%] sm:h-[35rem] md:w-[90%] md:h-[40rem] lg:w-[85%] lg:h-[45rem] xl:h-[50rem] 2xl:h-[55rem] mx-auto md:mt-8  object-cover  p-8'
 			/>
-			<p className='text-xl pt-4'>
-				Flaymze Creative Studios is a media and production services
-				company specialised in events media coverage, creative visuals
-				and commercials, movies and television. <br />
-				With an exceptionally skilled team and a reputation for
-				delivering quality work for our broad client base time after
-				time.
-			</p>
-			<div>
-				<div className=' py-16 lg:py-28 '>
-					<div className='px-4 sm:px-6 max-w-5xl py-2 lg:max-w-7xl  mx-auto md:flex  text-lg'>
-						<div className='flex-1'>
-							{' '}
-							<p className=' text-2xl font-medium'>TEAM</p>
-							<p className='md:w-[90%] my-4 pl-4'>
-								<span className='font-bold'>
-									King Peter Obi
-								</span>{' '}
-								- position
-							</p>
-							<p className='md:w-[90%] my-4 pl-4'>
-								<span className='font-bold'>Jerry</span> -
-								position
-							</p>
-							<p className='md:w-[90%] my-4 pl-4'>
-								<span className='font-bold'>Kelvin</span> -
-								position
-							</p>
-							<p className='md:w-[90%] my-4 pl-4'>
-								<span className='font-bold'>Chief</span> -
-								position
-							</p>
-						</div>
-						<div className='flex-1 justify-center mt-8 md:mt-0 '>
-							{' '}
-							<p className=' text-2xl font-medium '>
-								SPEAK WITH US
-							</p>
-							<div className='mx-auto w-fit pt-4'>
-								<Link
-									href={`tel:${phoneNumber}`}
-									className=' mx-auto w-fit'>
-									<p className='flex hover:text-primary mx-auto w-fit py-2'>
-										<Phone
-											size={20}
-											className='mr-2 mt-1'
-										/>{' '}
-										{phoneNumber}
-									</p>
-								</Link>
-								<Link
-									href={`mailto:${emailAddress}`}
-									className=' mx-auto w-fit'>
-									<p className='flex hover:text-primary mx-auto w-fit py-2'>
-										<Mail size={20} className='mr-2 mt-1' />{' '}
-										{emailAddress}
-									</p>
-								</Link>
+			{/* </div> */}
+
+			<div className=' px-4 sm:px-6 max-w-5xl py-2 bg-background lg:max-w-7xl  mx-auto  text-center'>
+				<p className='text-xl pt-4'>
+					Flaymze Creative Studios is a media and production services
+					company specialised in events media coverage, creative
+					visuals and commercials, movies and television. <br />
+					With an exceptionally skilled team and a reputation for
+					delivering quality work for our broad client base time after
+					time.
+				</p>
+				<div>
+					<div className=' py-16 lg:py-28 '>
+						<div className='px-4 sm:px-6 max-w-5xl py-2 lg:max-w-7xl  mx-auto md:flex  text-lg'>
+							<div className='flex-1'>
+								{' '}
+								<p className=' text-2xl font-medium'>TEAM</p>
+								<p className='md:w-[90%] my-4 pl-4'>
+									<span className='font-bold'>
+										King Peter Obi
+									</span>{' '}
+									- position
+								</p>
+								<p className='md:w-[90%] my-4 pl-4'>
+									<span className='font-bold'>Jerry</span> -
+									position
+								</p>
+								<p className='md:w-[90%] my-4 pl-4'>
+									<span className='font-bold'>Kelvin</span> -
+									position
+								</p>
+								<p className='md:w-[90%] my-4 pl-4'>
+									<span className='font-bold'>Chief</span> -
+									position
+								</p>
 							</div>
-						</div>
-						<div className='flex-1 justify-center mt-8 md:mt-0 '>
-							{' '}
-							<p className=' text-2xl font-medium'>
-								<span className='inline-block w-3 h-3 bg-primary rounded-full mr-1'></span>
-								SOCIAL
-							</p>
-							<div className='mx-auto w-fit mt-4'>
-								<Link
-									href='https://www.instagram.com/flaymzephotos?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
-									target='_blank'
-									className='mx-auto w-fit'>
-									<p className='flex hover:text-primary mx-auto w-fit font-bold text-lg '>
-										{/*<Phone size={20} className='mr-2 mt-1'/>{' '}*/}
-										Flaymze Photos
-									</p>
-								</Link>
-								<br />
-								<Link
-									href='https://www.instagram.com/flaymzeentertainment?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
-									target='_blank'
-									className='mx-auto w-fit'>
-									<p className='flex hover:text-primary mx-auto w-fit font-bold text-lg '>
-										{/*<Phone size={20} className='mr-2 mt-1'/>{' '}*/}
-										Flaymze Entertainment
-									</p>
-								</Link>
-								<br />
-								<Link
-									href='https://www.instagram.com/flaymzeweddings?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
-									target='_blank'
-									className='mx-auto w-fit'>
-									<p className='flex hover:text-primary mx-auto w-fit font-bold text-lg '>
-										{/*<Phone size={20} className='mr-2 mt-1'/>{' '}*/}
-										Flaymze Weddings
-									</p>
-								</Link>
-								<br />
-								{/*<Link*/}
-								{/*	href='https://wa.me/message/OZYFXEP5GXRHF1'*/}
-								{/*	target='_blank'>*/}
-								{/*	<p className='flex hover:text-primary'>*/}
-								{/*		<FaWhatsapp*/}
-								{/*			size={20}*/}
-								{/*			className='mr-2 mt-1'*/}
-								{/*		/>{' '}*/}
-								{/*		{phoneNumber}*/}
-								{/*	</p>*/}
-								{/*</Link>*/}
-								{/*<br/>*/}
-								{/*<Link href={`mailto:${emailAddress}`}>*/}
-								{/*	<p className='flex hover:text-primary'>*/}
-								{/*		<Mail size={20} className='mr-2 mt-1'/>{' '}*/}
-								{/*		{emailAddress}*/}
-								{/*	</p>*/}
-								{/*</Link>*/}
+							<div className='flex-1 justify-center mt-8 md:mt-0 '>
+								{' '}
+								<p className=' text-2xl font-medium '>
+									SPEAK WITH US
+								</p>
+								<div className='mx-auto w-fit pt-4'>
+									<Link
+										href={`tel:${phoneNumber}`}
+										className=' mx-auto w-fit'>
+										<p className='flex hover:text-primary mx-auto w-fit py-2'>
+											<Phone
+												size={20}
+												className='mr-2 mt-1'
+											/>{' '}
+											{phoneNumber}
+										</p>
+									</Link>
+									<Link
+										href={`mailto:${emailAddress}`}
+										className=' mx-auto w-fit'>
+										<p className='flex hover:text-primary mx-auto w-fit py-2'>
+											<Mail
+												size={20}
+												className='mr-2 mt-1'
+											/>{' '}
+											{emailAddress}
+										</p>
+									</Link>
+								</div>
+							</div>
+							<div className='flex-1 justify-center mt-8 md:mt-0 '>
+								{' '}
+								<p className=' text-2xl font-medium'>
+									<span className='inline-block w-3 h-3 bg-primary rounded-full mr-1'></span>
+									SOCIAL
+								</p>
+								<div className='mx-auto w-fit mt-4'>
+									<Link
+										href='https://www.instagram.com/flaymzephotos?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
+										target='_blank'
+										className='mx-auto w-fit'>
+										<p className='flex hover:text-primary mx-auto w-fit font-bold text-lg '>
+											{/*<Phone size={20} className='mr-2 mt-1'/>{' '}*/}
+											Flaymze Photos
+										</p>
+									</Link>
+									<br />
+									<Link
+										href='https://www.instagram.com/flaymzeentertainment?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
+										target='_blank'
+										className='mx-auto w-fit'>
+										<p className='flex hover:text-primary mx-auto w-fit font-bold text-lg '>
+											{/*<Phone size={20} className='mr-2 mt-1'/>{' '}*/}
+											Flaymze Entertainment
+										</p>
+									</Link>
+									<br />
+									<Link
+										href='https://www.instagram.com/flaymzeweddings?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='
+										target='_blank'
+										className='mx-auto w-fit'>
+										<p className='flex hover:text-primary mx-auto w-fit font-bold text-lg '>
+											{/*<Phone size={20} className='mr-2 mt-1'/>{' '}*/}
+											Flaymze Weddings
+										</p>
+									</Link>
+									<br />
+									{/*<Link*/}
+									{/*	href='https://wa.me/message/OZYFXEP5GXRHF1'*/}
+									{/*	target='_blank'>*/}
+									{/*	<p className='flex hover:text-primary'>*/}
+									{/*		<FaWhatsapp*/}
+									{/*			size={20}*/}
+									{/*			className='mr-2 mt-1'*/}
+									{/*		/>{' '}*/}
+									{/*		{phoneNumber}*/}
+									{/*	</p>*/}
+									{/*</Link>*/}
+									{/*<br/>*/}
+									{/*<Link href={`mailto:${emailAddress}`}>*/}
+									{/*	<p className='flex hover:text-primary'>*/}
+									{/*		<Mail size={20} className='mr-2 mt-1'/>{' '}*/}
+									{/*		{emailAddress}*/}
+									{/*	</p>*/}
+									{/*</Link>*/}
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
--- app/(site)/Services/page.tsx
+++ app/(site)/Services/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Image from 'next/image';
 
 async function getData() {
-	const query = `*[_type == "aboutImg"][0]`;
+	const query = `*[_type == "servicesImg"][0]`;
 	return await client.fetch(query);
 }
 
@@ -11,39 +11,45 @@ export default async function Services() {
 	const data = await getData();
 
 	return (
-		<div className=' px-4 sm:px-6 max-w-5xl py-2 bg-background lg:max-w-7xl  mx-auto  text-center'>
-			<h1 className='text-3xl font-medium pt-4 pb-4'>ABOUT US</h1>
+		<>
+			<h1 className='text-3xl font-medium pt-8 pb-4 text-center'>
+				ABOUT US
+			</h1>
+			{/* <div className=' relative w-[85%] mx-auto overflow-hidden pb-[46.25%]'> */}
 			<Image
 				src={urlFor(data.image).url()}
 				alt='Great Image'
 				width={450}
 				height={450}
 				priority
-				// className="embla__slide__img "
-				className=' w-full md:mt-8'
+				// className=' absolute top-0 left-0 w-full h-full object-cover '
+				className='w-full h-[30rem] sm:w-[95%] sm:h-[35rem] md:w-[90%] md:h-[40rem] lg:w-[85%] lg:h-[45rem] xl:h-[50rem] 2xl:h-[55rem] mx-auto md:mt-8  object-cover  p-8'
 			/>
-			<div className=' px-8 md:px-16 lg:px-36'>
-				<p className='pt-8 px-4 text-xl leading-relaxed '>
-					At FLAYMZE STUDIO, we provide expert service production for
-					shoots in Nigeria, captivating African locations, Canada,
-					and select European countries. Our capabilities span
-					corporate events, music videos, documentaries, weddings,
-					commercials, photo shoots, and more.
-				</p>
-				<p className='text-lg'>
-					The African narrative is changing, and our continent&apos;s
-					unique textures, colors, people, and untapped locations are
-					now an envy globally. Our seasoned team, comprising skilled
-					camera crews, producers, location managers, and production
-					designers, ensures top-notch production that meets
-					international standards.
-				</p>
-				<p>
-					Check out our portfolio and get inspired! Let&apos;s
-					collaborate to bring your vision to life. Contact us today
-					for tailored service production solutions.
-				</p>
+			<div className=' px-4 sm:px-6 max-w-5xl py-2 bg-background lg:max-w-7xl  mx-auto  text-center'>
+				<div className=' px-8 md:px-16 lg:px-36'>
+					<p className='pt-8 px-4 text-xl leading-relaxed '>
+						At FLAYMZE STUDIO, we provide expert service production
+						for shoots in Nigeria, captivating African locations,
+						Canada, and select European countries. Our capabilities
+						span corporate events, music videos, documentaries,
+						weddings, commercials, photo shoots, and more.
+					</p>
+					<p className='text-lg'>
+						The African narrative is changing, and our
+						continent&apos;s unique textures, colors, people, and
+						untapped locations are now an envy globally. Our
+						seasoned team, comprising skilled camera crews,
+						producers, location managers, and production designers,
+						ensures top-notch production that meets international
+						standards.
+					</p>
+					<p>
+						Check out our portfolio and get inspired! Let&apos;s
+						collaborate to bring your vision to life. Contact us
+						today for tailored service production solutions.
+					</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
